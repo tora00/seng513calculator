db4e4c0dea6520bf2e655409aea8276b6865f8fd
dazzu <navalkevin0@gmail.com>
2019-02-15 19:42:50 -0700

    Added more stuff
    - Finish javascript functions for calculator
    - Added bootstrap css on html file
    - Added handheld media compatibility for design

--- calcfunctions.js
+++ calcfunctions.js
@@ -1,29 +1,35 @@
-var newresult="";
-
 $(document).ready(function(){
+    let newresult="";
+    let storedfunction="";
     $("button").click(function() {
         let but = $(this).val();
-        if(but === 'C'){
-            newresult="";
+        if(but === 'CE'){
+            newresult=newresult.substring(0,newresult.length-1);
             $("#result").html(newresult);
         }
         else if(but !== '='){
-            newresult += but;
+            if(but === "Ans")
+                newresult+=storedfunction;
+            else if(isNaN(but) && newresult==="")
+                newresult=0+but;
+            else
+                newresult += but;
             $("#result").html(newresult);
         }
         else{
             try{
-                newresult = eval(newresult);    
+                newresult = eval(newresult).toString();
+                storedfunction = newresult;
+                $("#old-result").html(storedfunction);
                 $("#result").html(newresult);
             }
             catch(e){
                 if (e instanceof SyntaxError){
-                    $("#result").html("Undefined");
+                    $("#result").html("Invalid Experession!");
                 }
             }      
         }
         
     });
-
 });
 
